1a612aa6c65ac85e01d9dee38b4c2b985aa09f13
tjsm <mathincosmos@gmail.com>
2024-10-20 19:44:55 +0530

    change baseURL to deployed node instance

--- services/mood.ts
+++ services/mood.ts
@@ -1,20 +1,18 @@
 import axios from "axios";
-
 interface ResponseType {
   response: string;
 }
+
+const baseURL = "https://mood-tracker-0x33.onrender.com";
 
 const getInsights = async (
   mood: number,
   description: string
 ): Promise<string> => {
-  const insights = await axios.post<ResponseType>(
-    "http://localhost:3000/mood-insights",
-    {
-      mood,
-      description,
-    }
-  );
+  const insights = await axios.post<ResponseType>(`${baseURL}/mood-insights"`, {
+    mood,
+    description,
+  });
 
   return insights.data.response;
 };
